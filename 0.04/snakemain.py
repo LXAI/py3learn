5fcdd2a2d970562eccd8e235edcff2b10f067e58
liuxing <49858398+LXAI@users.noreply.github.com>
2019-09-23 22:57:24 +0800

    Update snakemain.py

--- 0.04/snakemain.py
+++ 0.04/snakemain.py
@@ -3,6 +3,13 @@ import random
 from collections import deque
 import numpy as np
 
+
+def getPositionFromMap(mapPos):
+    return (mapPos[0]*snakeNodeW,mapPos[1]*snakeNodeH)
+
+
+
+
 pygame.init()
 #code：刘兴
 snakeLen=10
@@ -13,7 +20,7 @@ gameMapSize=(50,50)
 snakeNodeW=int(width/gameMapSize[0])
 snakeNodeH=int(height/gameMapSize[1])
 gameMap=np.zeros(gameMapSize,dtype=np.int16)
-snakeShowPos=[0,0]
+
 
 screen = pygame.display.set_mode(size)
 snakeNodeSize=(snakeNodeW,snakeNodeH)
@@ -24,8 +31,15 @@ print(snakeNode.get_size())
 print(snakeNodeRect.width,snakeNodeRect.height)
 snakeNodeDeque=deque([])#蛇节点队列
 fpsClock = pygame.time.Clock()
-newNodeMapPos=[0,0]
-nowNodeMapPos=[0,0]
+[0,0]
+nowNodeMapPos=[gameMapSize[0]//2,gameMapSize[1]//2]
+newNodeMapPos=nowNodeMapPos
+nowNodePos=getPositionFromMap(nowNodeMapPos)
+snakeNodeRect.x=nowNodePos[0]
+snakeNodeRect.y=nowNodePos[1]
+snakeNodeDeque.append((snakeNode,snakeNodeRect,nowNodeMapPos))
+gameMap[tuple(nowNodeMapPos)]=1
+print(nowNodeMapPos)
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -33,37 +47,72 @@ while True:
             sys.exit()
     runSpeed = list(snakeNodeSize)
     runDirection=random.randint(1,4)#left,right,up,down
-    if runDirection==1:
-        if (newNodeMapPos[0]<0):
-            continue
-        runSpeed[1]=0 
-        runSpeed[0]=-runSpeed[0]
-    elif runDirection==2:
-        if (newNodeMapPos[1]>=gameMapSize[0]):
-            continue
-        runSpeed[1]=0     
-    elif runDirection==3:
-        runSpeed[0]=0
-        runSpeed[1]=-runSpeed[1]
-    elif runDirection==4:
-        runSpeed[0]=0     
-
+    
+    isContiune=True
+    for i in range(2):        
+        if runDirection==1 and isContiune :
+            newNodeMapPos[0]=nowNodeMapPos[0]-1
+            if newNodeMapPos[0]<0:
+                isContiune=True
+            elif gameMap[tuple(newNodeMapPos)]==0:
+                isContiune=False
+                runSpeed[1]=0 
+                runSpeed[0]=-runSpeed[0]
+                
+        if runDirection==2 and isContiune :
+            newNodeMapPos[0]=nowNodeMapPos[0]+1
+            if newNodeMapPos[0]>=gameMapSize[0]:
+                isContiune=True
+            elif gameMap[tuple(newNodeMapPos)]==0:
+                isContiune=False            
+                runSpeed[1]=0     
+            
+        if runDirection==3 and isContiune :
+            newNodeMapPos[1]=nowNodeMapPos[1]-1
+            if newNodeMapPos[1]<0 :
+                isContiune=True
+            elif gameMap[tuple(newNodeMapPos)]==0:
+                isContiune=False             
+                runSpeed[0]=0
+                runSpeed[1]=-runSpeed[1]
+        if runDirection==4 and isContiune :
+            newNodeMapPos[1]=nowNodeMapPos[1]+1        
+            if newNodeMapPos[1]>=gameMapSize[1]:
+                isContiune=True
+            elif gameMap[tuple(newNodeMapPos)]==0:
+                isContiune=False             
+                runSpeed[0]=0 
+        print("===")
+        print(nowNodeMapPos)
+        print(newNodeMapPos)
+        print("===")        
+        if not isContiune:
+            break
+        
+        
+    if isContiune:   
+        print("game over")
+        pygame.quit()
+        sys.exit()
+        
 
     snakeNodeRect= snakeNodeRect.move(runSpeed)
     snakeNodeHeadNew = snakeNode
     snakeNodeHeadNewRect= snakeNodeRect
-    snakeNodeDeque.append((snakeNodeHeadNew,snakeNodeHeadNewRect))
-
+    snakeNodeDeque.append((snakeNodeHeadNew,snakeNodeHeadNewRect,newNodeMapPos))
+    gameMap[tuple(newNodeMapPos)]=1 
+    nowNodeMapPos=newNodeMapPos
         
-    if len(snakeNodeDeque)>snakeLen:
+    if len(snakeNodeDeque)>snakeLen: 
         snakeNodeAbandon=snakeNodeDeque.popleft()
+        snakeNodeAbandonPos=snakeNodeAbandon[2]
+        gameMap[tuple(snakeNodeAbandonPos)]=0
         del snakeNodeAbandon
     
+
     screen.fill(black)   
     for snakeNodeData in snakeNodeDeque:
         screen.blit(snakeNodeData[0],snakeNodeData[1])
     pygame.display.update()
-    fpsClock.tick(5)
 
-def getPositionFromMap(mapPos):
-    return (mapPos[0]*snakeNodeW,mapPos[1]*snakeNodeH)
+    fpsClock.tick(5)
